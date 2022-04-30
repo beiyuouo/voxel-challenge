087d7a112cda10b372954b1ede0e2e4d068877d5
beiyuouo <beiyu233@gmail.com>
2022-04-30 21:45:06 +0800

    build: maze with bug

--- maze.py
+++ maze.py
@@ -6,67 +6,65 @@ scene = Scene(exposure=1)
 scene.set_floor(-0.05, (1.0, 1.0, 1.0))
 scene.set_background_color((1.0, 0, 0))
 
-height, width = 10, 10
-maze = ti.field(dtype=ti.i32, shape=(height, width))
-u_parent = ti.field(dtype=ti.i32, shape=(height * width))
-wall_set = ti.field(dtype=ti.i32, shape=(height * width, 2))
-dirs = [vec2(0, 1), vec2(1, 0), vec2(0, -1), vec2(-1, 0)]
+height, width = 15, 15
 
-dirs = ti.Vector.field(2, dtype=ti.f32, shape=(4))
-dirs[0], dirs[1], dirs[2], dirs[3] = vec2(0, 1), vec2(1, 0), vec2(0, -1), vec2(-1, 0)
-
-abled = ti.field(dtype=ti.i32, shape=(4, 2))
+u_parent = [_ for _ in range(height * width)]
 
 
-@ti.func
-def get_parent(x):
+def find(x):
     if u_parent[x] == x:
         return x
-    u_parent[x] = get_parent(u_parent[x])
+    else:
+        u_parent[x] = find(u_parent[x])
     return u_parent[x]
 
 
-@ti.func
 def union(x, y):
-    u_parent[get_parent(x)] = get_parent(y)
+    x, y = find(x), find(y)
+    if x != y:
+        u_parent[x] = y
 
 
-@ti.func
 def generate_maze(height, width, start_pos, end_pos):
-    maze[start_pos[0], start_pos[1]] = 0
-    maze[end_pos[0], end_pos[1]] = 0
-
-    for i in range(height * width):
-        u_parent[i] = i
-
-    itr = 0
+    maze_ = [[[0, 0, 0] for _ in range(width)] for _ in range(height)]
+    # maze_[start_pos[0]][start_pos[1]][0] = 0
+    # maze_[end_pos[0]][end_pos[1]][0] = 0
+    wall_set = []
     for i in range(height):
         for j in range(width):
-            if maze[i, j] == 1:
-                wall_set[itr, 0], wall_set[itr, 1] = i, j
-                itr += 1
+            if i + 1 < height:
+                wall_set.append((i, j, 1))
+            if j + 1 < width:
+                wall_set.append((i, j, 2))
 
-    # shuffle
-    for i in range(itr):
-        x, y = int(ti.random() * itr), int(ti.random() * itr)
-        temp = vec2(wall_set[x, 0], wall_set[x, 1])
-        wall_set[x, 0], wall_set[x, 1] = wall_set[y, 0], wall_set[y, 1]
-        wall_set[y, 0], wall_set[y, 1] = temp.x, temp.y
+    _x, _y = 0, 345
+    for i in range(1000):
+        _x, _y = (_x + 2333333) % len(wall_set), (_y + 2333333) % len(wall_set)
+        if _x != _y:
+            wall_set[_x], wall_set[_y] = wall_set[_y], wall_set[_x]
+    print(wall_set)
+    while len(wall_set) > 0:
+        pos = wall_set.pop()
+        x1, y1 = pos[0], pos[1]
+        if pos[2] == 1:
+            x2, y2 = x1 + 1, y1
+        else:
+            x2, y2 = x1, y1 + 1
+        if find(x1 * width + y1) != find(x2 * width + y2):
+            union(x1 * width + y1, x2 * width + y2)
+        else:
+            maze_[x1][y1][pos[2]] = 1
+    return maze_
 
-    for i in range(itr):
-        pos = vec2(wall_set[i, 0], wall_set[i, 1])
-        itr = 0
-        for idx in range(4):
-            pos_ = pos + dirs[idx]
-            if pos_.x >= 0 and pos_.x < height and pos_.y >= 0 and pos_.y < width:
-                abled[itr, 0], abled[itr, 1] = pos_.x, pos_.y
-                itr += 1
-        if itr > 1:
-            for it in range(1, itr):
-                if get_parent(abled[it, 0] * width +
-                              abled[it, 1]) != get_parent(abled[0, 0] * width + abled[0, 1]):
-                    union(abled[it, 0] * width + abled[it, 1], abled[0, 0] * width + abled[0, 1])
-                    maze[pos.x, pos.y] = 0
+
+maze_ = generate_maze(height, width, (0, 0), (width - 1, height - 1))
+for _ in maze_:
+    print(_)
+
+maze = ti.Vector.field(3, ti.i32, (height, width))
+for i in range(height):
+    for j in range(width):
+        maze[i, j] = maze_[i][j]
 
 
 @ti.func
@@ -77,12 +75,16 @@ def build_wall(pos, dir, len, color, color_noise):
 
 @ti.kernel
 def initialize_voxels():
-    generate_maze(10, 10, (0, 0), (9, 9))
-    for i, j in ti.ndrange(10, 10):
-        if maze[i][j] == 1:
-            scene.set_voxel((i, 1, j), 1, (0, 0, 0))
+    for i, j in ti.ndrange(height, width):
+        scene.set_voxel(vec3(i * 2, 1, j * 2), 2, vec3(1, 1, 1))
+        if maze[i, j][1] == 1:
+            scene.set_voxel(vec3(i * 2 + 1, 1, j * 2), 1, vec3(0.5, 0.5, 0.5))
         else:
-            scene.set_voxel((i, 1, j), 1, (1, 1, 1))
+            scene.set_voxel(vec3(i * 2 + 1, 1, j * 2), 1, vec3(1, 1, 1))
+        if maze[i, j][2] == 1:
+            scene.set_voxel(vec3(i * 2, 1, j * 2 + 1), 1, vec3(0.5, 0.5, 0.5))
+        else:
+            scene.set_voxel(vec3(i * 2, 1, j * 2 + 1), 1, vec3(1, 1, 1))
 
 
 initialize_voxels()
